d3791c9252b00f23811c5f1db1ae1a61bfc50349
Christopher Lynch <YOUR-EMAIL-ADDRESS>
2015-02-11 08:16:54 -0800

    added models and controllers

--- app/models/pick.rb
+++ app/models/pick.rb
@@ -1,2 +1,4 @@
 class Pick < ActiveRecord::Base
+	belongs_to :user
+  belongs_to :league
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+	has_many :picks
+  has_and_belongs_to_many :leagues 
+
+  
 end
