9ec390faffc3c0260fec6acd095c94dc29912828
Christopher Lynch <YOUR-EMAIL-ADDRESS>
2015-02-19 21:35:20 -0800

    pushed to heroku

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,19 @@ class UsersController < ApplicationController
 		
 	end
 
+	def show
+		@user = User.find(params[:id])
+		
+		
+	end
+
 	def new
 	@user = User.new
 
 		
 	end
+
+
 
 	def create
 		@user = User.new(user_params)
